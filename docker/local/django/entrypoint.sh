a02e8b05b21f671c76228be724f3ea10eeaa2402
MOHAMMAD Taushif Reza <112973122+TaushifReza@users.noreply.github.com>
2025-02-17 23:33:21 +0545

    Add custom auth with djoser and simplejwt

--- docker/local/django/entrypoint.sh
+++ docker/local/django/entrypoint.sh
@@ -22,7 +22,7 @@ while True:
             port="${POSTGRES_PORT}",
         )
         break
-    except psycopg2.OperationError as error:
+    except psycopg2.OperationalError as error:
         sys.stderr.write("Waiting for PostgreSQL to become available...\n")
         if time.time() - start > suggest_unrecoverable_after:
             sys.stderr.write(
